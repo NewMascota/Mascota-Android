7bc1e711d3332961c1fb317b306dfec290368957
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-11-02 18:28:09 +0900

    [CHORE] #2 - Init Gradle Setting

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Versions.gradleVersion}")
+        classpath(kotlin("gradle-plugin", version = Versions.kotlinVersion))
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlintVersion}")
+        classpath(ClassPathPlugins.hilt)
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> { debug.set(true) }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}

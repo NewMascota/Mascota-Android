679b90c8e6f2d613dbfac43a806635367cea1391
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-12-09 01:54:48 +0900

    [ADD] #2 - Init MainViewModel

--- app/src/main/java/org/mascota/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/MainViewModel.kt
@@ -2,6 +2,7 @@ package org.mascota.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel()
+class MainViewModel @Inject constructor() : ViewModel()

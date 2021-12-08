98885b9d5d67004d3de85333c9d89e2e95b9655c
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-12-08 22:26:48 +0900

    [CHORE] #2 - Lint Check

--- app/src/main/java/org/mascota/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/org/mascota/ui/viewmodel/MainViewModel.kt
@@ -1,7 +1,9 @@
 package org.mascota.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class MainViewModel : ViewModel() {
 
 }

1c1230f41585b450701b6fee07255788e3f1f3af
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-12-09 01:54:32 +0900

    [ADD] #2 - Init Activity

---
+++ app/src/main/java/org/mascota/ui/view/calendar/CalendarActivity.kt
@@ -0,0 +1,12 @@
+package org.mascota.ui.view.calendar
+
+import android.os.Bundle
+import org.mascota.mascota.R
+import org.mascota.mascota.databinding.ActivityCalendarBinding
+import org.mascota.ui.base.BindingActivity
+
+class CalendarActivity : BindingActivity<ActivityCalendarBinding>(R.layout.activity_calendar) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

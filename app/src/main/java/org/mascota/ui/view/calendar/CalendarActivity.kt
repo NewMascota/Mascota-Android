0af9485c539e9885349c915c430aae75bf041f4c
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-12-19 22:33:45 +0900

    [ADD] #7 - Init CalendarActivity

--- app/src/main/java/org/mascota/ui/view/calendar/CalendarActivity.kt
+++ app/src/main/java/org/mascota/ui/view/calendar/CalendarActivity.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import org.mascota.mascota.R
 import org.mascota.mascota.databinding.ActivityCalendarBinding
 import org.mascota.ui.base.BindingActivity
+import org.mascota.ui.view.calendar.adapter.YearAdapter
 
 class CalendarActivity : BindingActivity<ActivityCalendarBinding>(R.layout.activity_calendar) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    private fun initClickEvent() {
+        with(binding) {
+            clYear.setOnClickListener {
+                ibMore.isSelected = !ibMore.isSelected
+                isListVisible = ibMore.isSelected
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        val yearAdapter = YearAdapter()
+
+        yearAdapter.setItemClickListener { }
     }
 }

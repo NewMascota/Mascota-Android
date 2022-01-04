f623ad5a6b3ef177ede4bbf00a4a03c03129edaf
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-01-04 21:22:24 +0900

    [FEAT] #7 - Init CalendarActivity

--- app/src/main/java/org/mascota/ui/view/calendar/CalendarActivity.kt
+++ app/src/main/java/org/mascota/ui/view/calendar/CalendarActivity.kt
@@ -5,10 +5,18 @@ import org.mascota.mascota.R
 import org.mascota.mascota.databinding.ActivityCalendarBinding
 import org.mascota.ui.base.BindingActivity
 import org.mascota.ui.view.calendar.adapter.YearAdapter
+import org.mascota.ui.view.calendar.adapter.YearAdapter.Companion.MORE
+import java.util.Calendar
 
 class CalendarActivity : BindingActivity<ActivityCalendarBinding>(R.layout.activity_calendar) {
+    private val calendar = Calendar.getInstance()
+    private var year = calendar.get(Calendar.YEAR)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initUi()
+        initClickEvent()
+        initAdapter()
     }
 
     private fun initClickEvent() {
@@ -23,6 +31,38 @@ class CalendarActivity : BindingActivity<ActivityCalendarBinding>(R.layout.activ
     private fun initAdapter() {
         val yearAdapter = YearAdapter()
 
-        yearAdapter.setItemClickListener { }
+        yearAdapter.data = listOf(
+            year - 1,
+            year + 1,
+            MORE
+        )
+
+        yearAdapter.setItemClickListener {
+            with(binding) {
+                with(tvYear) {
+                    if (it != MORE) {
+                        text = "${it}년"
+                        year = it
+
+                        yearAdapter.data = listOf(
+                            year - 1,
+                            year + 1,
+                            MORE
+                        )
+                    }
+                    else
+                        text = getString(R.string.more)
+                }
+
+                ibMore.isSelected = !ibMore.isSelected
+                isListVisible = false
+            }
+        }
+
+        binding.rvYear.adapter = yearAdapter
+    }
+
+    private fun initUi() {
+        binding.tvYear.text = "${year}년"
     }
 }

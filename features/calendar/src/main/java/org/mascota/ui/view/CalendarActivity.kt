4c8ebf74babd297c37ba8656ffdbd97a5fad13c0
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-01-28 20:11:17 +0900

    [CHORE] #10 - Lint Check

--- features/calendar/src/main/java/org/mascota/ui/view/CalendarActivity.kt
+++ features/calendar/src/main/java/org/mascota/ui/view/CalendarActivity.kt
@@ -1,12 +1,12 @@
 package org.mascota.ui.view
 
 import android.os.Bundle
+import java.util.Calendar
 import org.mascota.R
 import org.mascota.core.base.BindingActivity
 import org.mascota.databinding.ActivityCalendarBinding
 import org.mascota.ui.view.adapter.YearAdapter
 import org.mascota.ui.view.adapter.YearAdapter.Companion.MORE
-import java.util.Calendar
 
 class CalendarActivity : BindingActivity<ActivityCalendarBinding>(R.layout.activity_calendar) {
     private val calendar = Calendar.getInstance()

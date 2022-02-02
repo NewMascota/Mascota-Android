81560285beeed7ad39f4c0e60343cab36c5fea44
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-02 10:13:05 +0900

    [CHORE] #29 - Update Etc File

--- features/user/src/test/java/org/mascota/ExampleUnitTest.kt
+++ features/user/src/test/java/org/mascota/ExampleUnitTest.kt
@@ -3,6 +3,11 @@ package org.mascota
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {

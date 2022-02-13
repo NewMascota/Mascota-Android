2ea1069f72b5349ab698b27a32f9ed8c1d87b99f
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-13 19:56:56 +0900

    [FEAT] #20 - Implement SignUpEnd Logic

--- features/user/src/main/java/org/mascota/user/ui/view/signup/SignUpEndFragment.kt
+++ features/user/src/main/java/org/mascota/user/ui/view/signup/SignUpEndFragment.kt
@@ -2,12 +2,23 @@ package org.mascota.user.ui.view.signup
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.mascota.core.base.BindingFragment
 import org.mascota.user.R
 import org.mascota.user.databinding.FragmentSignUpEndBinding
+import org.mascota.user.ui.viewmodel.UserViewModel
 
+@AndroidEntryPoint
 class SignUpEndFragment : BindingFragment<FragmentSignUpEndBinding>(R.layout.fragment_sign_up_end) {
+    private val viewModel: UserViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.tvId.text = viewModel.getEmail()
     }
 }

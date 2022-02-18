43b3aa273aafa0ca41a2f803205135df9aefea22
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-18 14:35:55 +0900

    [CHORE] #26 - Modify Data Module Gradle

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -17,36 +17,14 @@ dependencies {
     // Android Core
     implementation(KotlinDependencies.kotlin)
     implementation(AndroidXDependencies.coreKtx)
-    implementation(AndroidXDependencies.appCompat)
-    implementation(AndroidXDependencies.constraintLayout)
     implementation(AndroidXDependencies.coroutines)
-
-    // Material Design
-    implementation(MaterialDesignDependencies.materialDesign)
 
     // Dagger-Hilt
     implementation(AndroidXDependencies.hilt)
     kapt(KaptDependencies.hiltCompiler)
 
-    // Jetpack Navigation Component
-    implementation(AndroidXDependencies.navigationFragment)
-    implementation(AndroidXDependencies.navigationUI)
-
     // Jetpack Security
     implementation(AndroidXDependencies.security)
-
-    // Jetpack Fragment
-    implementation(AndroidXDependencies.fragment)
-
-    // Jetpack Lifecycle
-    implementation(AndroidXDependencies.coroutines)
-    implementation(AndroidXDependencies.lifeCycleKtx)
-    implementation(AndroidXDependencies.lifecycleJava8)
-
-    // ImageLoading Library
-    // Glide for general
-    implementation(ThirdPartyDependencies.glide)
-    kapt(KaptDependencies.glideCompiler)
 
     // Http Client Library
     implementation(ThirdPartyDependencies.retrofit)

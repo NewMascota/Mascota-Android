9bd59300aaf32d4fbc4405e755702ef122670a9a
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-19 19:38:38 +0900

    [FEAT] #9 - Init Custom Calendar

--- features/calendar/src/main/java/org/mascota/ui/view/custom/calendar/CalendarView.kt
+++ features/calendar/src/main/java/org/mascota/ui/view/custom/calendar/CalendarView.kt
@@ -1,4 +1,4 @@
-package org.mascota.ui.custom.calendar
+package org.mascota.ui.view.custom.calendar
 
 import android.content.Context
 import android.util.AttributeSet
@@ -9,10 +9,10 @@ import android.widget.LinearLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import org.mascota.mascota.R
-import org.mascota.mascota.databinding.ItemDateBinding
-import org.mascota.mascota.databinding.LayoutCalendarTopBinding
-import java.util.*
+import java.util.Calendar
+import org.mascota.calendar.R
+import org.mascota.calendar.databinding.ItemDateBinding
+import org.mascota.calendar.databinding.LayoutCalendarTopBinding
 
 class CalendarView(context: Context, attrs: AttributeSet? = null) : LinearLayout(context, attrs) {
     private val calendarTopView
@@ -20,8 +20,7 @@ class CalendarView(context: Context, attrs: AttributeSet? = null) : LinearLayout
 
     private lateinit var layoutCalendarTopBinding: LayoutCalendarTopBinding
 
-    private val nowCalendar = Calendar.getInstance(Locale.KOREA)
-    private var dateItemGetter: (() -> List<Boolean>)? = null
+    // private var dateItemGetter: (() -> List<Pair<Boolean, Boolean>>)? = null
     private var calendarGetter: (() -> Calendar)? = null
 
     private val dateItems = (1..42).map { _ ->
@@ -34,13 +33,13 @@ class CalendarView(context: Context, attrs: AttributeSet? = null) : LinearLayout
         calendarGetter = listener
     }
 
-    fun setDateItemGetter(listener: () -> List<Boolean>) {
-        dateItemGetter = listener
-    }
+    //    fun setDateItemGetter(listener: () -> List<Pair<Boolean, Boolean>>) {
+    //        dateItemGetter = listener
+    //    }
 
     private val outerLinearLayout = LinearLayout(context).apply {
         id = ViewCompat.generateViewId()
-        orientation = LinearLayout.VERTICAL
+        orientation = VERTICAL
         weightSum = 6f
 
         layoutParams = LayoutParams(
@@ -51,10 +50,10 @@ class CalendarView(context: Context, attrs: AttributeSet? = null) : LinearLayout
     private val innerLinearLayouts = (1..6).map {
         LinearLayout(context).apply {
             id = ViewCompat.generateViewId()
-            orientation = LinearLayout.HORIZONTAL
+            orientation = HORIZONTAL
             weightSum = 7f
 
-            layoutParams = LinearLayout.LayoutParams(
+            layoutParams = LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 1f
@@ -74,49 +73,52 @@ class CalendarView(context: Context, attrs: AttributeSet? = null) : LinearLayout
     }
 
     private fun updateUIWithDate() {
-        var pos = 0
-        var index = 31
-        val compCalendar = Calendar.getInstance(Locale.KOREA)
-        compCalendar.set(Calendar.DAY_OF_MONTH, 1)
-        //val dateItem = dateItemGetter?.invoke()
-        //val compCalendar = calendarGetter?.invoke()?.clone() as Calendar
-        //val curMonth = compCalendar.get(Calendar.MONTH)
+        // val dateItem = dateItemGetter?.invoke()
+        val compCalendar = requireNotNull(calendarGetter?.invoke())
+        val curMonth = compCalendar.get(Calendar.MONTH)
 
+        //        if (dateItem != null) {
+        //        index = dateItem.size
+        //        }
 
-        //if (dateItem != null) {
-        //index = dateItem.size
-        //}
-
-//        compCalendar.set(Calendar.DAY_OF_MONTH, 1)
-//        if (compCalendar.get(Calendar.DAY_OF_WEEK) == 1)
-//            compCalendar.add(Calendar.DAY_OF_MONTH, -6)
-//        else
-//            compCalendar.add(Calendar.DAY_OF_MONTH, 2 - compCalendar.get(Calendar.DAY_OF_WEEK))
+        layoutCalendarTopBinding.tvMonth.text = "${curMonth + 1}월"
+        compCalendar.set(Calendar.DAY_OF_WEEK, 1)
 
         innerLinearLayouts.forEach {
             it.removeAllViews()
         }
         outerLinearLayout.removeAllViews()
 
-        layoutCalendarTopBinding.tvMonth.text = "10월"
-        repeat(6) { row ->
-            val innerLinearLayout = innerLinearLayouts[row]
+        run {
+            repeat(6) { row ->
+                val innerLinearLayout = innerLinearLayouts[row]
+                outerLinearLayout.addView(innerLinearLayout)
 
-            outerLinearLayout.addView(innerLinearLayout)
+                repeat(7) { column ->
+                    val idx = row * 7 + column
+                    val item = dateItems[idx]
 
-            repeat(7) { column ->
-                val idx = row * 7 + column
-                val item = dateItems[idx]
+                    if (row == 0)
+                        item.line.isVisible = false
 
-                item.tvDay.text = compCalendar.get(Calendar.DAY_OF_MONTH).toString()
-                if (row == 0)
-                    item.line.isVisible = false
-                compCalendar.add(Calendar.DAY_OF_MONTH, 1)
-                innerLinearLayout.addView(
-                    item.root, LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f
+                    if (compCalendar.get(Calendar.MONTH) != curMonth)
+                        item.clDay.isVisible = false
+                    else
+                        item.tvDay.text = compCalendar.get(Calendar.DAY_OF_MONTH).toString()
+
+                    compCalendar.add(Calendar.DAY_OF_MONTH, 1)
+
+                    innerLinearLayout.addView(
+                        item.root, LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            1f
+                        )
                     )
-                )
+                }
+
+                if (compCalendar.get(Calendar.MONTH) == curMonth + 1)
+                    return@run
             }
         }
     }

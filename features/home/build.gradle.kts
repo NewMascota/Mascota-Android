d03a4321c495ccd0c1eaa5f6f498231d2c5847a2
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-19 19:39:29 +0900

    [CHORE] #9 - Add Calendar Module Dependency in Home Module

--- features/home/build.gradle.kts
+++ features/home/build.gradle.kts
@@ -14,6 +14,7 @@ android {
 
 dependencies {
     implementation(project(":core:"))
+    implementation(project(":features:calendar:"))
 
     // Android Core
     implementation(KotlinDependencies.kotlin)

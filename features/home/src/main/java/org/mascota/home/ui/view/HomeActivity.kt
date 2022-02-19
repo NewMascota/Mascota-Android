f69dbe4a939cf2d8b483a0ec8a56271d1b44c1ee
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-19 19:36:25 +0900

    [ADD] #9 - Add StartCalendar In HomeActivity

--- features/home/src/main/java/org/mascota/home/ui/view/HomeActivity.kt
+++ features/home/src/main/java/org/mascota/home/ui/view/HomeActivity.kt
@@ -1,6 +1,8 @@
 package org.mascota.home.ui.view
 
+import android.content.Intent
 import android.os.Bundle
+import org.mascota.calendar.ui.view.CalendarActivity
 import org.mascota.core.base.BindingActivity
 import org.mascota.home.R
 import org.mascota.home.databinding.ActivityHomeBinding
@@ -8,5 +10,17 @@ import org.mascota.home.databinding.ActivityHomeBinding
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initClickEvent()
+    }
+
+    private fun initClickEvent() {
+        binding.ibCalendar.setOnClickListener {
+            startCalendar()
+        }
+    }
+
+    private fun startCalendar() {
+        startActivity(Intent(this, CalendarActivity::class.java))
     }
 }

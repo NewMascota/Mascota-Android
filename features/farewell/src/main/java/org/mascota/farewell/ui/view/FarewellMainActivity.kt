1ef6ba17a2e9c0590467e133d3e3c5adb2f3a8ee
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-02-26 22:21:15 +0900

    [ADD] #12 - Init FarewellActivity

---
+++ features/farewell/src/main/java/org/mascota/farewell/ui/view/FarewellMainActivity.kt
@@ -0,0 +1,12 @@
+package org.mascota.farewell.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mascota.farewell.R
+
+class FarewellMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_farewell_main)
+    }
+}
